674ebe46bd626a68f801cf906521fa09b35240e6
Mayank Agrawal <mayk03jun@gmail.com>
2025-06-06 09:25:00 +0530

    next config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Don't bundle Prisma Client on the server
+      config.externals.push('@prisma/client')
+    }
+    return config
+  }
 };
 
 export default nextConfig;
